e266657eee03c353df088cb79a35b1ddf4d0d923
Dillon L. Peterson <dillonpeterson@mac.com>
2017-06-17 22:18:39 -0500

    Added some product/validation tests

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -4,4 +4,43 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # This is where you specify fixtures for the test to load into the database before 
+  # RUN
+  fixtures :products
+  test "product attributes must not be empty" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:image_url].any?
+  	assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "The dog", description: "yyy", image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url" do
+    ok = %w[fred.gif fred.jpg fred.png FRED.JPG fred.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gig/mor fred.gif.more]
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  private 
+    def new_product(image_url)
+      Product.new(title: "My New Book", description: "It is a good book", image_url: image_url, price: 1)
+    end
 end
