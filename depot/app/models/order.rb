02dbf8b0b26491e451f9bf32466cd3134552d47c
Dillon L. Peterson <dillonpeterson@mac.com>
2017-06-19 11:55:34 -0500

    Add Validation to Order

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
-  enum pay_type: {
+  validates :name, :email, :address, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+  enum pay_types: {
     "Check" => 0,
     "Credit Card" => 1,
     "Purchase Order" => 2
