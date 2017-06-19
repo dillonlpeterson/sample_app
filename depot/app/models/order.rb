8273aea62b001589dec53853b7a46678ddc84b67
Dillon L. Peterson <dillonpeterson@mac.com>
2017-06-19 12:08:21 -0500

    Add way to transfer line_items from cart to order. And to place order

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
   validates :name, :email, :address, presence: true
   validates :pay_type, inclusion: pay_types.keys
   enum pay_types: {
@@ -6,4 +7,11 @@ class Order < ApplicationRecord
     "Credit Card" => 1,
     "Purchase Order" => 2
   }
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
