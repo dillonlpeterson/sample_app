5120494a44d5c86a041e7e321cdcc7ed75e34fd2
Dillon L. Peterson <dillonpeterson@mac.com>
2017-06-19 15:28:04 -0500

    Just made some integration tests

--- depot/test/mailers/order_mailer_test.rb
+++ depot/test/mailers/order_mailer_test.rb
@@ -2,19 +2,16 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
+    mail = OrderMailer.received(orders(:one))
     assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
+    mail = OrderMailer.shipped(orders(:one))
     assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["dave@example.com"], mail.from
   end
-
 end
