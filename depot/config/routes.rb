a8866e7993c85acdbcd67a1f278ceb1f28b9018d
Dillon L. Peterson <dillonpeterson@mac.com>
2017-06-20 17:26:48 -0500

    We now have SessionsController and AdminController

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get 'admin' => 'admin#index'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  controller :sessions do 
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   resources :users
   resources :orders
   resources :line_items 
   resources :carts
   root 'store#index', as: 'store_index'
+
   resources :products do
     get :who_bought, on: :member
   end
