1d357631e902cd1bcac61091616a431c981f3b11
Dillon L. Peterson <dillonpeterson@mac.com>
2017-06-20 17:34:03 -0500

    All Tests passing. Now time for Iteration I3. Must restrict administrative access.

--- depot/app/controllers/sessions_controller.rb
+++ depot/app/controllers/sessions_controller.rb
@@ -10,10 +10,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:name])
+    user = User.find_by(name:params[:name])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
-      redirect to admin_url
+      redirect_to admin_url
     else
       redirect_to login_url, alert: 'Invalid user/password combination'
     end
--- depot/test/controllers/sessions_controller_test.rb
+++ depot/test/controllers/sessions_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should prompt for login" do
-    get login_url 
-    assert_response :success 
-  end
-
   test "should login" do
     dave = users(:one)
-    post login_url, params: {name: dave.password, password: "wrong"}
+    post login_url, params: {name: dave.name, password: 'secret'}
     assert_redirected_to admin_url 
     assert_equal dave.id, session[:user_id]
   end
